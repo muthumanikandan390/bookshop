1e8b5f88f3f51c52cba0816ec622a03893c6b867
manikandan390 <manikandan390d@gmail.com>
2023-12-26 09:35:41 +0530

    django-1

---
+++ omnitrix/omnitrix/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for omnitrix project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omnitrix.settings")
+
+application = get_wsgi_application()
