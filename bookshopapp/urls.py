2d5fd7a653cb359a6514b745d75d29e1ad42104f
manikandan390 <manikandan390d@gmail.com>
2023-12-26 18:38:26 +0530

    django-6

--- bookshopapp/urls.py
+++ bookshopapp/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from bookshopapp import views
 urlpatterns = [
     path('',views.base,name = "base"),
+    path('loginpage/',views.loginpage,name = "login-page"),
 ]
--- bookshopapp/views.py
+++ bookshopapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     return render(request,'bookshopapp/base.html')
+
+def loginpage(request):
+    return render(request,'bookshopapp/loginpage.html')
