baf0e4ef9d03ed0d5037e7c031615fb907c0cdd2
manikandan390 <manikandan390d@gmail.com>
2023-12-26 12:51:07 +0530

    django-4

---
+++ static/assets/js/app-invoice-print.js
@@ -0,0 +1,25 @@
+/**
+ * Invoice Print
+ */
+
+'use strict';
+
+(function () {
+  // var css = '@page { size: landscape; }',
+  //   head = document.head || document.getElementsByTagName('head')[0],
+  //   style = document.createElement('style');
+
+  //   style.type = 'text/css';
+  //   style.media = 'print';
+
+  //   if (style.styleSheet){
+  //     style.styleSheet.cssText = css;
+  //   } else {
+  //     style.appendChild(document.createTextNode(css));
+  //   }
+
+  //   head.appendChild(style);
+
+window.print();
+  // window.print();
+})();
