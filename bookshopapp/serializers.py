c3c183d8c7c9807b1917ee3e4b5202a0474215f7
manikandan390 <manikandan390d@gmail.com>
2024-01-05 13:50:26 +0530

    django-19

--- bookshopapp/serializers.py
+++ bookshopapp/serializers.py
@@ -2,6 +2,13 @@ from rest_framework import serializers
 from . models import Book
 
 class BookSerializer(serializers.ModelSerializer):
+    truncated_description = serializers.SerializerMethodField()
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = fields = ['id', 'book_id', 'description', 'truncated_description', 'name', 'author', 'price', 'currency', 'category', 'image_path']
+
+    def get_truncated_description(self, obj):
+        max_length = 100
+        if len(obj.description) > max_length:
+            return obj.description[:max_length] + '...'
+        return obj.description
