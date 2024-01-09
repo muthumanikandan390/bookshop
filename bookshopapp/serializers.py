2baaf9ccdae1575846f968f5d1798ef1030b2f51
manikandan390 <manikandan390d@gmail.com>
2024-01-09 20:17:16 +0530

    django-23

--- bookshopapp/serializers.py
+++ bookshopapp/serializers.py
@@ -1,14 +1,25 @@
 from rest_framework import serializers
-from . models import Book
+from . models import Book,CartItem,Cart
 
 class BookSerializer(serializers.ModelSerializer):
     truncated_description = serializers.SerializerMethodField()
     class Meta:
         model = Book
-        fields = fields = ['id', 'book_id', 'description', 'truncated_description', 'name', 'author', 'price', 'currency', 'category', 'image_path']
+        fields = ['id', 'book_id', 'description', 'truncated_description', 'name', 'author', 'price', 'currency', 'category', 'image_path']
 
     def get_truncated_description(self, obj):
         max_length = 100
         if len(obj.description) > max_length:
             return obj.description[:max_length] + '...'
         return obj.description
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        fields = ['id','cart_id','book_id','quantity']
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ['id','created_at','user_id']
+        
--- bookshopapp/views.py
+++ bookshopapp/views.py
@@ -35,15 +35,34 @@ def signup(request):
         form = SignUpForm
     return render(request,'bookshopapp/registerpage.html',{'form':form})
 
+
 def login_auth(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
-        user = Userprofile.objects.get(username = username , password = password)
-        return render(request,'bookshopapp/loginpage.html',{'username':username})
+        try:
+            user = Userprofile.objects.get(username=username, password=password)
+            request.session['user_id'] = user.id  # Storing user ID in the session
+            print(f"############:{user.id}")
+            request.session['mailid'] = user.email
+            return render(request, 'bookshopapp/loginpage.html', {'username': username})  # Redirect to base.html on success
+        except Userprofile.DoesNotExist:
+            return render(request, 'bookshopapp/base.html', {'error': 'Invalid username or password'})
+    return render(request, 'bookshopapp/login_valid.html')
+
+
+
+""" user_id = request.session.get('user_id')
+use this to use the id in our different function
+
+"""
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+
 
 
